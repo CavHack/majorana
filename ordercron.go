1bc4bac7be547459fba619e7c17b68e0ecbe4055
Karl Whitford <cevb@protonmail.com>
2019-07-08 03:33:37 -0400

    ordercron is a cron-friendly order abstraction
    The issue here is with goroutine subquery commands inside the compiler. See, it will be easier to abstract the dynamic heap of "orders" or "commands" in JIT fashion if there is no Unix Epoch Interval inference; at the boolean representation of a decision tree; the issue here is penalizing by tilmestep; the dispatcher hill climbs to the next optimum.

--- ordercron.go
+++ ordercron.go
@@ -1,1 +1,43 @@
 
+package zama-monte-mod
+
+
+
+// New returns a new Cron like order runner, in the Local time zone.
+func New() *Ordercron {
+        return NewWithLocation(time.Now().Location())
+}
+
+// NewWithLocation returns a new runner.
+func NewWithLocation(location *time.Location) *Ordercron {
+        return &Ordercron{
+                orders:    nil,
+                add:      make(chan *Entry),
+                stop:     make(chan struct{}),
+                snapshot: make(chan []*Entry),
+                running:  false,
+                ErrorLog: nil,
+                location: location,
+        }
+}
+
+// A wrapper that turns a func() into a cron.Job
+type FuncDispatcher func()
+
+func (f FuncDispatcher) Run() { f() }
+
+// AddFunc adds a func to the order dispatched on the given shelf
+func (oc *Ordercron) AddFunc(spec string, cmd func()) error {
+        return oc.AddOrder(spec, FuncDispatch(cmd))
+}
+
+// AddJob adds a Job to the Cron to be run on the given schedule.
+func (oc *Ordercron) AddOrderDispatch(spec string, cmd Dispatch) error {
+        shelf, err := Parse(spec)
+        if err != nil {
+                return err
+        }
+        oc.Schelf(schedule, cmd)
+        return nil
+}
+
